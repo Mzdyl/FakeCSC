76f1db88599525478cf83126be8d328cec3610f2
fei-ke <jinyang656@qq.com>
2022-01-27 13:03:30 +0800

    Initial Commit

---
+++ app/src/main/java/com/fei_ke/demo/XposedMod.java
@@ -0,0 +1,12 @@
+package com.fei_ke.demo;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedMod implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    }
+
+}
